652bdcd8025ea5ecaf0f0201cc2c6fbadf7ce75e
mtgto <hogerappa@gmail.com>
2013-03-07 01:02:11 +0900

    Add definition of table "pagelinks"

--- gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
+++ gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
@@ -2,11 +2,12 @@ package net.mtgto.pediaroute.domain
 
 import scala.slick.driver.MySQLDriver.simple._
 
+import java.util.Arrays
+
 class DataGenerateService {
   def generate(): Unit = {
     val (titles, idToIndexMap) = loadPages()
-    println(titles.toList)
-    println(idToIndexMap)
+    val (forwardLinks, backwardLinks) = loadPageLinks(titles, idToIndexMap)
   }
 
   def loadPages(): (Array[String], Map[Int, Int]) = {
@@ -14,16 +15,39 @@ class DataGenerateService {
       val query = for {
         page <- Pages if page.namespace === 0L && page.isRedirect === false
       } yield (page.id, page.title)
-      val pageIdTitleList: List[(Int, String)] = query.take(10).list.map {
+      val pageIdTitleList: List[(Int, String)] = query/*.take(10)*/.list.map {
         case (id, title) => (id, new String(title, "UTF-8"))
       }
       val titles: Array[String] = pageIdTitleList.map {
         case (id, title) => title
       }.toArray[String].sorted
       val idToIndexMap: Map[Int, Int] = pageIdTitleList.map {
-        case (id, title) => (id, java.util.Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title))
+        case (id, title) => (id, Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title))
       }.toMap[Int, Int]
       (titles, idToIndexMap)
+    }
+  }
+
+  def loadPageLinks(titles: Array[String], idToIndexMap: Map[Int, Int]): (Array[Seq[Int]], Array[Seq[Int]]) = {
+    Database.forURL("jdbc:mysql://localhost/wikipedia", driver = "com.mysql.jdbc.Driver", user = "user") withSession { implicit session: Session =>
+      val query = for {
+        pageLink <- PageLinks if pageLink.namespace === 0L
+      } yield (pageLink.from, pageLink.title)
+      val forwardLinks: Array[Seq[Int]] = titles.map(_ => Seq.empty[Int])
+      val backwardLinks: Array[Seq[Int]] = titles.map(_ => Seq.empty[Int])
+
+      query.take(10).foreach {
+        case (fromId, titleBytes) => {
+          val title = new String(titleBytes, "UTF-8")
+          val fromIndex = idToIndexMap.getOrElse(fromId, -1)
+          val toIndex = Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title)
+          if (fromIndex >= 0 && toIndex >= 0) {
+            forwardLinks(fromIndex) +:= toIndex
+            backwardLinks(toIndex) +:= fromIndex
+          }
+        }
+      }
+      (forwardLinks, backwardLinks)
     }
   }
 }
---
+++ gen/src/main/scala/net/mtgto/pediaroute/domain/PageLinks.scala
@@ -0,0 +1,11 @@
+package net.mtgto.pediaroute.domain
+
+import scala.slick.driver.MySQLDriver.simple._
+
+object PageLinks extends Table[(Int, Long, Array[Byte])]("pagelinks") {
+  def from = column[Int]("pl_from", O.PrimaryKey) // This is the primary key column
+  def namespace = column[Long]("pl_namespace")
+  def title = column[Array[Byte]]("pl_title")
+  // Every table needs a * projection with the same type as the table's type parameter
+  def * = from ~ namespace ~ title
+}
