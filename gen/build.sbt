9a3d9dc3c11f69ea7f254112708a21beeea40db2
mtgto <hogerappa@gmail.com>
2013-03-08 07:41:49 +0900

    Fix to use generated file for second execution.
    OutOfMemoryError 18693 s

--- gen/build.sbt
+++ gen/build.sbt
@@ -9,6 +9,7 @@ scalaVersion := "2.10.0"
 
 libraryDependencies := Seq(
   "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.2",
+  "com.github.scala-incubator.io" %% "scala-io-file" % "0.4.2",
   "com.typesafe.slick" %% "slick" % "1.0.0",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
   "mysql" % "mysql-connector-java" % "5.1.23",
--- gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
+++ gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
@@ -1,44 +1,75 @@
 package net.mtgto.pediaroute.domain
 
 import scala.slick.driver.MySQLDriver.simple._
+import scalax.file.Path
 import scalax.io.{Resource, Output, Input, OutputConverter}
-import scalax.io._
 import Resource._
 
 import java.util.Arrays
 
 class DataGenerateService {
   def generate(): Unit = {
-    val (titles, idToIndexMap) = loadPages()
-    val (forwardLinks, backwardLinks) = loadPageLinks(titles, idToIndexMap)
-    writeTitles(titles)
+    //val titleToIds: Array[(String, Int)] = Array(("AAA", 123), ("BBB", 45), ("CCC", 678))
+    val titleToIdsPath = Path("title.dat")
+    val titleToIds: Array[(String, Int)] =
+      if (titleToIdsPath.exists) {
+        readTitleToIds
+      } else {
+        val load = loadPages()
+        writeTitleToIds(load)
+        load
+      }
+    // id to index
+    val idToIndexMap: Map[Int, Int] = titleToIds.map(_._2).zipWithIndex.toMap
+    val (forwardLinks, backwardLinks) = loadPageLinks(idToIndexMap, titleToIds)
     writeForwardLinks(forwardLinks)
     writeBackwardLinks(backwardLinks)
   }
 
-  def writeTitles(titles: Seq[String]): Unit = {
+  def writeTitleToIds(titleToIds: Array[(String, Int)]): Unit = {
     val output: Output = Resource.fromFile("title.dat")
-    output.writeStrings(titles, "\n")
+    for {
+      processor <- output.outputProcessor
+      out = processor.asOutput
+    } {
+      titleToIds.foreach {
+        case (title, id) =>
+          out.write(id.toString + "," + title + "\n")
+      }
+    }
   }
 
-  def readTitles(): Array[String] = {
+  def readTitleToIds(): Array[(String, Int)] = {
     val input: Input = Resource.fromFile("title.dat")
-    input.lines().toArray
+    input.lines().map { line =>
+      val (idStr, wordWithComma) = line.span(_ != ',')
+      (wordWithComma.tail, idStr.toInt)
+    }.toArray[(String, Int)]
   }
 
   def writeForwardLinks(links: Array[Seq[Int]]): Unit = {
     val output = Resource.fromFile("forwardlinks.dat")
-    links.foreach { ary =>
-      output.writeStrings(ary.map(_.toString), ",")
-      output.write("\n")
+    for {
+      processor <- output.outputProcessor
+      out = processor.asOutput
+    } {
+      links.foreach { ary =>
+        out.writeStrings(ary.map(_.toString), ",")
+        out.write("\n")
+      }
     }
   }
 
   def writeBackwardLinks(links: Array[Seq[Int]]): Unit = {
     val output = Resource.fromFile("backwardlinks.dat")
-    links.foreach { ary =>
-      output.writeStrings(ary.map(_.toString), ",")
-      output.write("\n")
+    for {
+      processor <- output.outputProcessor
+      out = processor.asOutput
+    } {
+      links.foreach { ary =>
+        out.writeStrings(ary.map(_.toString), ",")
+        out.write("\n")
+      }
     }
   }
 
@@ -47,37 +78,30 @@ class DataGenerateService {
     println(input.lines().toList)
   }
 
-  def loadPages(): (Array[String], Map[Int, Int]) = {
+  def loadPages(): (Array[(String, Int)]) = {
     Database.forURL("jdbc:mysql://localhost/wikipedia", driver = "com.mysql.jdbc.Driver", user = "user") withSession { implicit session: Session =>
       val query = for {
         page <- Pages if page.namespace === 0L && page.isRedirect === false
       } yield (page.id, page.title)
-      val pageIdTitleList: List[(Int, String)] = query/*.take(10)*/.list.map {
-        case (id, title) => (id, new String(title, "UTF-8"))
-      }
-      val titles: Array[String] = pageIdTitleList.map {
-        case (id, title) => title
-      }.toArray[String].sorted
-      val idToIndexMap: Map[Int, Int] = pageIdTitleList.map {
-        case (id, title) => (id, Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title))
-      }.toMap[Int, Int]
-      (titles, idToIndexMap)
+      query.list.map {
+        case (id, title) => (new String(title, "UTF-8"), id)
+      }.toArray[(String, Int)].sortBy(_._1)
     }
   }
 
-  def loadPageLinks(titles: Array[String], idToIndexMap: Map[Int, Int]): (Array[Seq[Int]], Array[Seq[Int]]) = {
+  def loadPageLinks(idToIndexMap: Map[Int, Int], titleToIds: Array[(String, Int)]): (Array[Seq[Int]], Array[Seq[Int]]) = {
     Database.forURL("jdbc:mysql://localhost/wikipedia", driver = "com.mysql.jdbc.Driver", user = "user") withSession { implicit session: Session =>
       val query = for {
-        pageLink <- PageLinks if pageLink.namespace === 0L
-      } yield (pageLink.from, pageLink.title)
-      val forwardLinks: Array[Seq[Int]] = titles.map(_ => Seq.empty[Int])
-      val backwardLinks: Array[Seq[Int]] = titles.map(_ => Seq.empty[Int])
-      for (i <- 0 until 6534; subquery = query.drop(i*10000).take(10000)) {
+        pageLink <- PageLinks
+        page <- Pages if pageLink.title === page.title && pageLink.namespace === 0L && page.namespace === 0L && page.isRedirect === false
+      } yield (pageLink.from, page.id)
+      val forwardLinks: Array[Seq[Int]] = titleToIds.map(_ => Seq.empty[Int])
+      val backwardLinks: Array[Seq[Int]] = titleToIds.map(_ => Seq.empty[Int])
+      for (i <- 0 until 4949; subquery = query.drop(i*10000).take(10000)) {
         subquery.foreach {
-          case (fromId, titleBytes) => {
-            val title = new String(titleBytes, "UTF-8")
+          case (fromId, toId) => {
             val fromIndex = idToIndexMap.getOrElse(fromId, -1)
-            val toIndex = Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title)
+            val toIndex = idToIndexMap.getOrElse(toId, -1)
             if (fromIndex >= 0 && toIndex >= 0) {
               forwardLinks(fromIndex) +:= toIndex
               backwardLinks(toIndex) +:= fromIndex
