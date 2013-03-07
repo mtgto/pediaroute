39d111b3366b3f9e5358c0266322a95b58e61f4e
mtgto <hogerappa@gmail.com>
2013-03-07 23:18:50 +0900

    Add to dump link map.
    OutOfMemoryError happen. Total time: 14653 s.

--- gen/build.sbt
+++ gen/build.sbt
@@ -8,6 +8,7 @@ version := "0.1.0-SNAPSHOT"
 scalaVersion := "2.10.0"
 
 libraryDependencies := Seq(
+  "com.github.scala-incubator.io" %% "scala-io-core" % "0.4.2",
   "com.typesafe.slick" %% "slick" % "1.0.0",
   "org.slf4j" % "slf4j-nop" % "1.6.4",
   "mysql" % "mysql-connector-java" % "5.1.23",
--- gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
+++ gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
@@ -1,6 +1,9 @@
 package net.mtgto.pediaroute.domain
 
 import scala.slick.driver.MySQLDriver.simple._
+import scalax.io.{Resource, Output, Input, OutputConverter}
+import scalax.io._
+import Resource._
 
 import java.util.Arrays
 
@@ -8,6 +11,40 @@ class DataGenerateService {
   def generate(): Unit = {
     val (titles, idToIndexMap) = loadPages()
     val (forwardLinks, backwardLinks) = loadPageLinks(titles, idToIndexMap)
+    writeTitles(titles)
+    writeForwardLinks(forwardLinks)
+    writeBackwardLinks(backwardLinks)
+  }
+
+  def writeTitles(titles: Seq[String]): Unit = {
+    val output: Output = Resource.fromFile("title.dat")
+    output.writeStrings(titles, "\n")
+  }
+
+  def readTitles(): Array[String] = {
+    val input: Input = Resource.fromFile("title.dat")
+    input.lines().toArray
+  }
+
+  def writeForwardLinks(links: Array[Seq[Int]]): Unit = {
+    val output = Resource.fromFile("forwardlinks.dat")
+    links.foreach { ary =>
+      output.writeStrings(ary.map(_.toString), ",")
+      output.write("\n")
+    }
+  }
+
+  def writeBackwardLinks(links: Array[Seq[Int]]): Unit = {
+    val output = Resource.fromFile("backwardlinks.dat")
+    links.foreach { ary =>
+      output.writeStrings(ary.map(_.toString), ",")
+      output.write("\n")
+    }
+  }
+
+  def readForwardLinks() = {
+    val input: Input = Resource.fromFile("forwardlinks.dat")
+    println(input.lines().toList)
   }
 
   def loadPages(): (Array[String], Map[Int, Int]) = {
@@ -35,15 +72,16 @@ class DataGenerateService {
       } yield (pageLink.from, pageLink.title)
       val forwardLinks: Array[Seq[Int]] = titles.map(_ => Seq.empty[Int])
       val backwardLinks: Array[Seq[Int]] = titles.map(_ => Seq.empty[Int])
-
-      query.take(10).foreach {
-        case (fromId, titleBytes) => {
-          val title = new String(titleBytes, "UTF-8")
-          val fromIndex = idToIndexMap.getOrElse(fromId, -1)
-          val toIndex = Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title)
-          if (fromIndex >= 0 && toIndex >= 0) {
-            forwardLinks(fromIndex) +:= toIndex
-            backwardLinks(toIndex) +:= fromIndex
+      for (i <- 0 until 6534; subquery = query.drop(i*10000).take(10000)) {
+        subquery.foreach {
+          case (fromId, titleBytes) => {
+            val title = new String(titleBytes, "UTF-8")
+            val fromIndex = idToIndexMap.getOrElse(fromId, -1)
+            val toIndex = Arrays.binarySearch(titles.asInstanceOf[Array[Object]], title)
+            if (fromIndex >= 0 && toIndex >= 0) {
+              forwardLinks(fromIndex) +:= toIndex
+              backwardLinks(toIndex) +:= fromIndex
+            }
           }
         }
       }
