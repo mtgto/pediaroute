7aa70b076e2470d35eabc69e315da2a037629948
mtgto <hogerappa@gmail.com>
2013-03-08 23:59:26 +0900

    To use array for optimization
    4GB memory and 1395 s

--- gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
+++ gen/src/main/scala/net/mtgto/pediaroute/domain/DataGenerateService.scala
@@ -21,10 +21,10 @@ class DataGenerateService {
       }
     // id to index
     val idToIndexMap: Map[Int, Int] = titleToIds.map(_._2).zipWithIndex.toMap
-    val links: Array[(Int, Int)] = loadPageLinks(idToIndexMap)
+    val links: Array[Long] = loadPageLinks(idToIndexMap)
     println("num of links = " + links.length)
-    writeForwardLinks(titleToIds.length, links.sortBy(_._1))
-    writeBackwardLinks(titleToIds.length, links.sortBy(_._2))
+    writeForwardLinks(titleToIds.length, links.sortBy(_ % 4294967296L))
+    writeBackwardLinks(titleToIds.length, links.sortBy(_ / 4294967296L))
   }
 
   def writeTitleToIds(titleToIds: Array[(String, Int)]): Unit = {
@@ -48,46 +48,48 @@ class DataGenerateService {
     }.toArray[(String, Int)]
   }
 
-  def writeForwardLinks(indexCount: Int, links: Array[(Int, Int)]): Unit = {
+  def writeForwardLinks(indexCount: Int, links: Array[Long]): Unit = {
     val output = Resource.fromFile("forwardlinks.dat")
     for {
       processor <- output.outputProcessor
       out = processor.asOutput
     } {
-      var row = 0
-      for (index <- 0 until indexCount) {
-        var colIndex = 0
-        for (i <- row until links.length if links(i)._1 == index) {
-          if (colIndex != 0) {
-            out.write(",")
+      var lastIndex = 0
+      links.foreach { link =>
+        val fromIndex = (link % 4294967296L).toInt
+        val toIndex = (link / 4294967296L).toInt
+        if (lastIndex < fromIndex) {
+          for (i <- lastIndex until fromIndex) {
+            out.write("\n")
           }
-          out.write(links(i)._2.toString)
-          colIndex += 1
-          row += 1
+          lastIndex = fromIndex
+        } else {
+          out.write(",")
         }
-        out.write("\n")
+        out.write(toIndex.toString)
       }
     }
   }
 
-  def writeBackwardLinks(indexCount: Int, links: Array[(Int, Int)]): Unit = {
+  def writeBackwardLinks(indexCount: Int, links: Array[Long]): Unit = {
     val output = Resource.fromFile("backwardlinks.dat")
     for {
       processor <- output.outputProcessor
       out = processor.asOutput
     } {
-      var row = 0
-      for (index <- 0 until indexCount) {
-        var colIndex = 0
-        for (i <- row until links.length if links(i)._2 == index) {
-          if (colIndex != 0) {
-            out.write(",")
+      var lastIndex = 0
+      links.foreach { link =>
+        val fromIndex = (link % 4294967296L).toInt
+        val toIndex = (link / 4294967296L).toInt
+        if (lastIndex < toIndex) {
+          for (i <- lastIndex until toIndex) {
+            out.write("\n")
           }
-          out.write(links(i)._1.toString)
-          colIndex += 1
-          row += 1
+          lastIndex = toIndex
+        } else {
+          out.write(",")
         }
-        out.write("\n")
+        out.write(fromIndex.toString)
       }
     }
   }
@@ -108,23 +110,24 @@ class DataGenerateService {
     }
   }
 
-  def loadPageLinks(idToIndexMap: Map[Int, Int]): Array[(Int, Int)] = {
+  def loadPageLinks(idToIndexMap: Map[Int, Int]): Array[Long] = {
     Database.forURL("jdbc:mysql://localhost/wikipedia", driver = "com.mysql.jdbc.Driver", user = "user") withSession { implicit session: Session =>
       val rowCount = 49486640
       val perRowCount = 10000000
-      val links = new Array[(Int, Int)](rowCount)
+      val links = new Array[Long](rowCount)
       val query = for {
         pageLink <- PageLinks
         page <- Pages if pageLink.title === page.title && pageLink.namespace === 0L && page.namespace === 0L && page.isRedirect === false
       } yield (pageLink.from, page.id)
       var index = 0
       for (i <- 0 until rowCount by perRowCount; subquery = query.drop(i).take(perRowCount)) {
+        println(i)
         subquery.foreach {
           case (fromId, toId) => {
             val fromIndex = idToIndexMap.getOrElse(fromId, -1)
             val toIndex = idToIndexMap.getOrElse(toId, -1)
             if (fromIndex >= 0 && toIndex >= 0) {
-              links(index) = (fromIndex, toIndex)
+              links(index) = fromIndex + toIndex.toLong * 4294967296L
               index += 1
             }
           }
