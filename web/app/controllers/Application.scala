2a31e8e1e7935124f9410e76d4b1c2d05e3dca99
mtgto <hogerappa@gmail.com>
2013-03-10 01:27:40 +0900

    Add search implementation for web.

--- web/app/controllers/Application.scala
+++ web/app/controllers/Application.scala
@@ -1,11 +1,34 @@
 package controllers
 
+import net.mtgto.pediaroute.domain.{SearchService, Query}
+
 import play.api._
 import play.api.mvc._
 import play.api.data.Form
 import play.api.data.Forms._
+import play.api.libs.concurrent.Execution.Implicits._
+
+import concurrent.Future
+
+import scalax.io.{Resource, Input}
 
 object Application extends Controller {
+  private val titleFilePath = getConfiguration("pediaroute.title_file")
+
+  private val forwardLinkFilePath = getConfiguration("pediaroute.forward_link_file")
+
+  private val backwardLinkFilePath = getConfiguration("pediaroute.backward_link_file")
+
+  private val titles: Array[String] = readTitles(titleFilePath)
+
+  private val titleMap: Map[String, Int] = titles.zipWithIndex.toMap
+
+  private val forwardLinks: Array[Array[Int]] = readForwardLinks(forwardLinkFilePath, titles.length)
+
+  private val backwardLinks: Array[Array[Int]] = readBackwardLinks(backwardLinkFilePath, titles.length)
+
+  protected[this] val searchService: SearchService = new SearchService(titleMap, titles, forwardLinks, backwardLinks)
+
   protected[this] val searchForm = Form(
     tuple(
       "wordFrom" -> nonEmptyText,
@@ -17,15 +40,59 @@ object Application extends Controller {
     Ok(views.html.index(searchForm))
   }
   
-  def search = Action { implicit request =>
-    searchForm.bindFromRequest.fold(
-      formWithErrors =>
-        BadRequest(views.html.index(formWithErrors)),
-      success => success match {
-        // Either[Seq[String], String] Left: Answer, Right: Reason of failure
-        case (from, to) =>
-          Ok(views.html.search(from, to, new Left[Seq[String], String](Seq("ほげ", "fuga", "12345")), 123.45))
+  def search(wordFrom: String, wordTo: String) = Action { implicit request =>
+    if (!searchService.isTitleExists(wordFrom)) {
+      Ok(views.html.search(wordFrom, wordTo, new Right(wordFrom + "というページがないみたい"), 0.0))
+    } else if (!searchService.isTitleExists(wordTo)) {
+      Ok(views.html.search(wordFrom, wordTo, new Right(wordTo + "というページがないみたい"), 0.0))
+    } else {
+      val startTime = System.currentTimeMillis
+      val futureQuery: Future[Option[Query]] = Future {
+        searchService.find(wordFrom, wordTo)
       }
-    )
+      Async {
+        futureQuery.map { query =>
+          val result: Either[Seq[String], String] = query match {
+            case Some(query) => new Left(query.way)
+            case _ => new Right("6回のリンクじゃ見つからなかった…ごめんね！") // todo ページがないとき
+          }
+          val endTime = System.currentTimeMillis
+          Ok(views.html.search(wordFrom, wordTo, result, (endTime-startTime).toDouble/1000))
+        }
+      }
+    }
+  }
+
+  private def getConfiguration(name: String): String = Play.current.configuration.getString(name).get
+
+  protected[this] def readTitles(filePath: String): Array[String] = {
+    val input: Input = Resource.fromFile(filePath)
+    input.lines().map { line =>
+      line.dropWhile(_ != ',').tail
+    }.toArray[String]
+  }
+
+  protected[this] def readForwardLinks(filePath: String, pageCount: Int): Array[Array[Int]] = {
+    val links: Array[Array[Int]] = new Array[Array[Int]](pageCount)
+    val input: Input = Resource.fromFile(filePath)
+    var i = 0
+    input.lines().foreach { line =>
+      links(i) = if (line.length == 0) Array.empty[Int] else line.split(",").map(_.toInt)
+      i += 1
+    }
+    for (j <- i until pageCount) links(j) = Array.empty[Int]
+    links
+  }
+
+  protected[this] def readBackwardLinks(filePath: String, pageCount: Int): Array[Array[Int]] = {
+    val links: Array[Array[Int]] = new Array[Array[Int]](pageCount)
+    val input: Input = Resource.fromFile(filePath)
+    var i = 0
+    input.lines().foreach { line =>
+      links(i) = if (line.length == 0) Array.empty[Int] else line.split(",").map(_.toInt)
+      i += 1
+    }
+    for (j <- i until pageCount) links(j) = Array.empty[Int]
+    links
   }
 }
--- web/project/Build.scala
+++ web/project/Build.scala
@@ -8,14 +8,15 @@ object ApplicationBuild extends Build {
   val appVersion      = "1.0-SNAPSHOT"
 
   val appDependencies = Seq(
-    // Add your project dependencies here,
-    jdbc,
-    anorm
+//    "net.mtgto" %% "pediaroute-core" % "0.1.0-SNAPSHOT"
   )
 
+  lazy val core = Project(id = "core", base = file("modules/core")).settings(
+    scalaVersion := "2.10.0"
+  )
 
   val main = play.Project(appName, appVersion, appDependencies).settings(
     // Add your own project settings here      
-  )
+  ).dependsOn(core)
 
 }
