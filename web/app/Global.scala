2ef5c32d45cecacf339736f1b9037f0636b3b437
mtgto <hogerappa@gmail.com>
2013-03-10 02:22:06 +0900

    Add access logger.

---
+++ web/app/Global.scala
@@ -0,0 +1,23 @@
+import play.api._
+import play.api.mvc._
+
+import org.joda.time._
+
+object Global extends WithFilters(AccessLog) {
+  override def onStart(app: Application) {
+    Logger.info("Application has started")
+  }  
+  
+  override def onStop(app: Application) {
+    Logger.info("Application shutdown...")
+  }
+}
+
+object AccessLog extends Filter {
+  override def apply(next: RequestHeader => Result)(request: RequestHeader): Result = {
+    val result = next(request)
+//    play.Logger.info(request + "\n\t => " + result)
+    play.Logger.info("%s - guest [%s] \"%s %s %s\"".format(request.remoteAddress, new DateTime(), request.method, request.uri, request.version))
+    result
+  }
+}
